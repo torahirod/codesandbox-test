350d9e7c82f0ddce212b96bbe492f9a20c6059f9
torahirod <54745972+torahirod@users.noreply.github.com>
2021-06-18 00:01:18 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import "./styles.css";
+/**
+ * アロー関数
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 従来のやり方
+function fnc1(str) {
+  return str + "1111";
+}
+
+console.log(fnc1("abc"));
+
+// 変数に関数を入れることも可
+const fnc2 = function (str) {
+  return str + "222";
+};
+
+console.log(fnc2("cde"));
+
+// アロー関数
+const fnc3 = (str) => {
+  return str + "333";
+};
+
+console.log(fnc3("jkl"));
+
+// アロー関数
+// 引数が1つの場合、()がなくてもよい
+const fnc4 = (str) => {
+  return str + "444";
+};
+
+console.log(fnc4("opq"));
+
+// アロー関数
+// 処理が1行で終わり、それを返却するような場合は{}とreturn文は不要
+const fnc5 = (str) => str + "555";
+console.log(fnc5("xyz"));
+
+const fnc6 = (x, y) => {
+  return x + y;
+};
+
+console.log(fnc6(1, 2));
